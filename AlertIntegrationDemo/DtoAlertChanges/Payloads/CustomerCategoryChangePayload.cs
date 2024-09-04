bec00e563578e9d08037a257d13fa7d57c7262d1
Jakob Kramhøft <jjk@idq.dk>
2024-09-04 09:56:55 +0200

    Add handling of customerCategoryChanged to change reader Add Debug project to see current customer in the mock tenant system (sqlite db)

---
+++ AlertIntegrationDemo/DtoAlertChanges/Payloads/CustomerCategoryChangePayload.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BiQ.AlertIntegrationDemo.DtoAlertChanges
+{
+    public class CustomerCategoryChangePayload
+    {
+        public string? ExistingCustomerCategory { get; set; }
+        public string? ProposedCustomerCategory { get; set; }
+        public DateTimeOffset? CustomerCategoryChangeDate { get; set; }
+    }
+}
